91faebcc62171fc53a7c3062ba6a35f47ed47008
YongJun Cho <whdydwns73@naver.com>
2024-11-16 12:31:01 +0900

    test commit

--- src/main/java/com/devsign/Login_Form/controller/LoginController.java
+++ src/main/java/com/devsign/Login_Form/controller/LoginController.java
@@ -6,4 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
+    @GetMapping
+    public String login() {
+        return "login";
+    }
 }

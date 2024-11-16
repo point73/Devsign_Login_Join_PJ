f04c26c01af83ad8175388054e17f949807a60ca
YongJun Cho <whdydwns73@naver.com>
2024-11-16 12:28:00 +0900

    test commit

--- src/main/java/com/devsign/Login_Form/controller/LoginController.java
+++ src/main/java/com/devsign/Login_Form/controller/LoginController.java
@@ -1,10 +1,13 @@
 package com.devsign.Login_Form.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
-
-
+    @GetMapping
+    public String login() {
+        return "login";
+    }
 }

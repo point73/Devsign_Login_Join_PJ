fc538a4c9b6a1b722e0edb286285cfdd119d9612
YongJun Cho <whdydwns73@naver.com>
2024-11-16 15:17:57 +0900

    thymeleaf 디렉터리 구조 변화 ë

--- src/main/java/com/devsign/Login_Form/LoginFormApplication.java
+++ src/main/java/com/devsign/Login_Form/LoginFormApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LoginFormApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LoginFormApplication.class, args);
 	}
-
 }
